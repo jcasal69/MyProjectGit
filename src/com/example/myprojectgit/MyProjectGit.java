d9a3e185dc365aebd58079a0980953fdde1e738b
John Casal <johncasal@msn.com>
2024-11-21 23:52:12 -0600

    My Project Git Test Commit 2

--- src/com/example/myprojectgit/MyProjectGit.java
+++ src/com/example/myprojectgit/MyProjectGit.java
@@ -3,6 +3,7 @@ package com.example.myprojectgit;
 public class MyProjectGit {
 
     public static void main(String[] args) {
-        System.out.println("This is my project git!");
+        System.out.println("This is my project git! 1");
+        System.out.println("This is my project git! 2");
     }
 }
